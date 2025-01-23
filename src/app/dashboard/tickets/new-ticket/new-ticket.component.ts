eb862e1001f4b9f087e2f1038211e589551ee6e1
imHarish03 <harishhari302@gmail.com>
2025-01-23 23:56:17 +0530

    Simple ngbinding for ticket info

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,15 +1,23 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class NewTicketComponent {
+
+  titile!:string;
+  request!:string;
+
+onSubmit() {
+  console.log("Submitted"+this.titile+"-"+this.request);
+}
 
 }
