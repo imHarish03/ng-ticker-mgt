4229fcc7e1f2fb8ac736496bb1dba82edeecbdfc
imHarish03 <harishhari302@gmail.com>
2025-01-25 17:23:44 +0530

    #132-134 Load element using @contentchild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -11,18 +11,28 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit,AfterViewInit{
 
-//@ViewChild('form') form?:ElementRef<HTMLFormElement>;
+@ViewChild('form') form?:ElementRef<HTMLFormElement>;
 @ViewChild('titleInput') title?:ElementRef<string>;
 
-private form=viewChild.required<ElementRef<HTMLFormElement>>('form');
+//private form=viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+ngOnInit(): void {
+  console.log("After  OnInit");
+  console.log(this.form?.nativeElement);
+}
+
+ngAfterViewInit(): void {
+ console.log("After ViewInit");
+ console.log(this.form?.nativeElement);
+}
 
 onSubmit(titleInput:string, requestInput:string) {
   console.log("Submitted Title: "+titleInput+", Request: "+requestInput);
   console.log("Titile loaded via viewchild: "+this.title?.nativeElement);
-  //this.form?.nativeElement.reset();
-   this.form().nativeElement.reset();
+  this.form?.nativeElement.reset();
+   //this.form().nativeElement.reset();
 }
 
 }
